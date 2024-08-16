23cbe7a24bc12414dc0282ecc8f622b68a0b3be3
Anton Goncharov <42060678+AntonGoncharov1999@users.noreply.github.com>
2024-08-17 02:51:02 +0500

    1 гет запрос
    Hello World

---
+++ src/main/java/com/example/G_Buy/GBuyApplication.java
@@ -0,0 +1,13 @@
+package com.example.G_Buy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GBuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/G_Buy/GBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.G_Buy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
